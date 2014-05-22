2ded71dd103fa4b8eeab595f97de587b96d07ebf
danielepo <daniele.pozzobon@telit.com>
2014-05-22 23:34:47 +0200

    working serviceconnectorimpl

--- src/ServiceConnectorImpl.php
+++ src/ServiceConnectorImpl.php
@@ -1,14 +1,43 @@
 <?php
-class ServiceConnectorDummy implements ServiceConnector
+
+class ServiceConnectorImpl implements ServiceConnector
 {
+
+  private $host = "http://rest.mercuriosistemi.com/api/portali/lignanoit/presenti/hotel";
+  private $username = "Daniele.Pozzobon";
+  private $password = "546219819";
+
   public function getAllHotels()
   {
-    
+
+    $xml = $this->execGet($this->host);
+    $entries = $xml->xpath("//entry");
+    return $entries;
   }
 
   public function getHotelInformation($href)
   {
+    return $this->execGet($href);
     
   }
 
+  private function execGet($host)
+  {
+    $url_handler = curl_init($host);
+    curl_setopt($url_handler, CURLOPT_HTTPHEADER, array('Accept: text/xml', 'Content-Type: application/json'));
+    curl_setopt($url_handler, CURLOPT_HEADER, 1);
+    curl_setopt($url_handler, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+    curl_setopt($url_handler, CURLOPT_TIMEOUT, 30);
+    curl_setopt($url_handler, CURLOPT_RETURNTRANSFER, TRUE);
+    $return = curl_exec($url_handler);
+    curl_close($url_handler);
+    
+    return $this->extractXml($return);
+  }
+  private function extractXml($responseString){
+    $xmlpos = strpos($responseString, '<?xml version="1.0" encoding="UTF-8"?');
+    $xmlstring = substr($responseString, $xmlpos);
+    return new SimpleXMLElement($xmlstring);
+    
+  }
 }
---
+++ tests/ServiceConnectorImplTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+class ServiceConnectorImplTest extends PHPUnit_Framework_TestCase{
+  public function testReadsWebService(){
+    
+    $serviceConnector = new ServiceConnectorImpl();
+    $this->assertEquals(147, count($serviceConnector->getAllHotels()));
+  }
+
+  public function testFetchesOneHotel(){
+    $serviceConnector = new ServiceConnectorImpl();
+    $hotelxml = $serviceConnector->getHotelInformation("http://rest.mercuriosistemi.com/api/hotel/hotel/1072");
+    
+    $this->assertEquals("Hotel Rio", trim((string)$hotelxml->esercizio->nome));
+    $this->assertEquals("http://www.hotelriolignano.com", trim((string)$hotelxml->esercizio->web));
+    $this->assertEquals("info@hotelriolignano.com", trim((string)$hotelxml->esercizio->email));
+    $this->assertEquals("+39.0431.71280", trim((string)$hotelxml->esercizio->telefono));
+    $this->assertEquals("+39.0431.71637", trim((string)$hotelxml->esercizio->fax));
+  }
+}
